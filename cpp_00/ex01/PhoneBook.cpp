ab8552b79b2b15d56659f6f1d80dd11f5b57f0a2
Vidus Sivanathan <148643381+v1dusss@users.noreply.github.com>
2025-01-13 22:11:33 +0100

    cpp00: ex01: improved add_contact function

--- cpp_00/ex01/PhoneBook.cpp
+++ cpp_00/ex01/PhoneBook.cpp
@@ -10,21 +10,76 @@ PhoneBook::~PhoneBook()
 {
 }
 
+bool is_alpha(const std::string& str) {
+	for (char c : str) {
+		if (!std::isalpha(c)) {
+			return false;
+		}
+	}
+	return true;
+}
+
+bool is_numeric(const std::string& str) {
+	for (char c : str) {
+		if (!std::isdigit(c)) {
+			return false;
+		}
+	}
+	return true;
+}
+
 void PhoneBook::add_contact(int index)
 {
 	std::string input;
-	std::cout << "First name: ";
-	std::getline(std::cin, input);
+
+	// First name
+	do {
+		std::cout << "First name: ";
+		std::getline(std::cin, input);
+		if (input.empty())
+		{
+			std::cout << "Input cannot be empty" << std::endl;
+		}
+		else if (!is_alpha(input)) {
+			std::cout << "Invalid input. Please use alphabetical letters only.\n";
+		}
+	} while (!is_alpha(input) || input.empty());
 	contacts[index].set_first_name(input);
-	std::cout << "Last name: ";
-	std::getline(std::cin, input);
+
+	// Last name
+	do {
+		std::cout << "Last name: ";
+		std::getline(std::cin, input);
+		if (input.empty())
+		{
+			std::cout << "Input cannot be empty" << std::endl;
+		}
+		else if (!is_alpha(input)) {
+			std::cout << "Invalid input. Please use alphabetical letters only.\n";
+		}
+	} while (!is_alpha(input) || input.empty());
 	contacts[index].set_last_name(input);
+
+	// Nickname
 	std::cout << "Nickname: ";
 	std::getline(std::cin, input);
 	contacts[index].set_nickname(input);
-	std::cout << "Phone number: ";
-	std::getline(std::cin, input);
+
+	// Phone number
+	do {
+		std::cout << "Phone number: ";
+		std::getline(std::cin, input);
+		if (input.empty())
+		{
+			std::cout << "Input cannot be empty" << std::endl;
+		}
+		else if (!is_numeric(input)) {
+			std::cout << "Invalid input. Please use numerical digits only.\n";
+		}
+	} while (!is_numeric(input) || input.empty());
 	contacts[index].set_phone_number(input);
+
+	// Darkest secret
 	std::cout << "Darkest secret: ";
 	std::getline(std::cin, input);
 	contacts[index].set_darkest_secret(input);
