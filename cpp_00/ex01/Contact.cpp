db32a834ec0f857ebe629e1ce7b7263a66f21180
Vidus Sivanathan <148643381+v1dusss@users.noreply.github.com>
2025-01-13 21:23:03 +0100

    cpp00: ex01: added Contact files

---
+++ cpp_00/ex01/Contact.cpp
@@ -0,0 +1,60 @@
+
+#include "Contact.hpp"
+
+Contact::Contact()
+{
+}
+
+Contact::~Contact()
+{
+}
+
+void Contact::set_first_name(std::string first_name)
+{
+	this->first_name = first_name;
+}
+
+std::string Contact::get_first_name()
+{
+	return (this->first_name);
+}
+
+void Contact::set_last_name(std::string last_name)
+{
+	this->last_name = last_name;
+}
+
+std::string Contact::get_last_name()
+{
+	return (this->last_name);
+}
+
+void Contact::set_nickname(std::string nickname)
+{
+	this->nickname = nickname;
+}
+
+std::string Contact::get_nickname()
+{
+	return (this->nickname);
+}
+
+void Contact::set_phone_number(std::string phone_number)
+{
+	this->phone_number = phone_number;
+}
+
+std::string Contact::get_phone_number()
+{
+	return (this->phone_number);
+}
+
+void Contact::set_darkest_secret(std::string darkest_secret)
+{
+	this->darkest_secret = darkest_secret;
+}
+
+std::string Contact::get_darkest_secret()
+{
+	return (this->darkest_secret);
+}
