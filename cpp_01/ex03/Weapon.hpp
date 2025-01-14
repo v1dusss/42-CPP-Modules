d931b0927d5943c7a7138475dd06294d4c7d048e
v1dusss <vsivanat@student.42heilbronn.de>
2025-01-15 00:44:59 +0100

    cpp01: ex03: Weapon class implementation

---
+++ cpp_01/ex03/Weapon.hpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+#pragma once
+
+class Weapon
+{
+private:
+	std::string type;
+
+public:
+	Weapon(std::string type);
+	~Weapon();
+	const std::string &getType();
+	void setType(std::string type);
+};
