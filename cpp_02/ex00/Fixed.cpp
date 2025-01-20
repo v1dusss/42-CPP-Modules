99c8994d230a12d75eabb928d3b02bb25bbce2ae
Vidus Sivanathan <148643381+v1dusss@users.noreply.github.com>
2025-01-20 20:18:09 +0100

    cpp02: ex00: Implement Fixed class with constructors, assignment operator, and member functions

--- cpp_02/ex00/Fixed.cpp
+++ cpp_02/ex00/Fixed.cpp
@@ -1,1 +1,35 @@
- 
+#include "Fixed.hpp"
+
+Fixed::Fixed()
+{
+	std::cout << "Default constructor called" << std::endl;
+}
+
+Fixed::Fixed(const Fixed &fixed)
+{
+	std::cout << "Copy constructor called" << std::endl;
+	this->fixedPointValue = fixed.fixedPointValue;
+}
+
+Fixed& Fixed::operator=(const Fixed &fixed)
+{
+	std::cout << "Assignation operator called" << std::endl;
+	this->fixedPointValue = fixed.getRawBits();
+	return (*this);
+}
+
+Fixed::~Fixed()
+{
+	std::cout << "Destructor called" << std::endl;
+}
+
+int Fixed::getRawBits(void) const
+{
+	std::cout << "getRawBits member function called" << std::endl;
+	return (this->fixedPointValue);
+}
+
+void Fixed::setRawBits(int const raw)
+{
+	this->fixedPointValue = raw;
+}
