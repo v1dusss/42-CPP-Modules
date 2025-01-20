b070d5676250c8c7c67abcb7d21e1c2e75825154
Vidus Sivanathan <148643381+v1dusss@users.noreply.github.com>
2025-01-20 19:35:45 +0100

    cpp01: ex06: added switch cases with fallthrough to print out the comments

--- cpp_01/ex06/Harl.cpp
+++ cpp_01/ex06/Harl.cpp
@@ -10,22 +10,26 @@ Harl::~Harl()
 
 void Harl::debug(void)
 {
-	std::cout << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-special ketchup burger. I really do!" << std::endl;
+	std::cout << "[ DEBUG ]\n";
+	std::cout << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-special ketchup burger. I really do!\n\n";
 }
 
 void Harl::info(void)
 {
-	std::cout << "I cannot believe adding extra bacon costs more money. You didn’t put enough bacon in my burger! If you did, I wouldn’t be asking for more!" << std::endl;
+	std::cout << "[ INFO ]\n";
+	std::cout << "I cannot believe adding extra bacon costs more money. You didn’t put enough bacon in my burger! If you did, I wouldn’t be asking for more!\n\n";
 }
 
 void Harl::warning(void)
 {
-	std::cout << "I think I deserve to have some extra bacon for free. I’ve been coming for years whereas you started working here since last month." << std::endl;
+	std::cout << "[ WARNING ]\n";
+	std::cout << "I think I deserve to have some extra bacon for free. I’ve been coming for years whereas you started working here since last month.\n\n";
 }
 
 void Harl::error(void)
 {
-	std::cout << "This is unacceptable! I want to speak to the manager now." << std::endl;
+	std::cout << "[ ERROR ]\n";
+	std::cout << "This is unacceptable! I want to speak to the manager now." << "\n\n";
 }
 
 void Harl::complain(std::string level)
@@ -37,9 +41,25 @@ void Harl::complain(std::string level)
 		&Harl::error
 	};
 	std::string arr[4] = { "DEBUG", "INFO", "WARNING", "ERROR" };
-	for (int i = 0; i < 4; i++)
+
+	int i;
+	for (i = 0; i < 4; i++)
 	{
 		if (level == arr[i])
-			(this->*functions[i])();
+			break;
+	}
+	switch (i) {
+		case 0:
+			(this->*functions[0])();
+			[[fallthrough]];
+		case 1:
+			(this->*functions[1])();
+			[[fallthrough]];
+		case 2:
+			(this->*functions[2])();
+			[[fallthrough]];
+		case 3:
+			(this->*functions[3])();
+			break;
 	}
 }
