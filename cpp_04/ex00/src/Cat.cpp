57bab007f0bdae80699c37cc2916b40889cdf678
Vidus Sivanathan <148643381+v1dusss@users.noreply.github.com>
2025-02-05 23:37:35 +0100

    cpp04: ex00: Implement Cat class with constructors, destructor, and makeSound method

---
+++ cpp_04/ex00/src/Cat.cpp
@@ -0,0 +1,26 @@
+#include "Cat.hpp"
+
+Cat::Cat() : Animal("Cat")
+{
+	std::cout << "Cat constructor called" << std::endl;
+}
+
+Cat::Cat(const Cat &copy) : Animal(copy)
+{
+	std::cout << "Cat copy constructor called" << std::endl;
+}
+
+Cat::~Cat()
+{
+	std::cout << "Cat destructor called" << std::endl;
+}
+
+Cat& Cat::operator=(const Cat &assign)
+{
+	std::cout << "Cat assignation operator called" << std::endl;
+}
+
+void Cat::makeSound() const
+{
+	std::cout << "Meow Meow" << std::endl;
+}
