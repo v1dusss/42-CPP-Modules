18c2c4935a4d586133988193ccf492c40b3d790e
Vidus Sivanathan <148643381+v1dusss@users.noreply.github.com>
2025-02-05 23:20:55 +0100

    cpp04: ex00: Implement Animal class

---
+++ cpp_04/ex00/include/Animal.hpp
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <iostream>
+
+class Animal
+{
+protected:
+	std::string type;
+
+public:
+	Animal();
+	Animal(std::string type);
+	Animal(const Animal &copy);
+	virtual ~Animal();
+	Animal &operator=(const Animal &assign);
+};
---
+++ cpp_04/ex00/src/Animal.cpp
@@ -0,0 +1,25 @@
+#include "Animal.hpp"
+
+Animal::Animal()
+{
+}
+
+Animal::Animal(std::string type)
+{
+	this->type = type;
+}
+
+Animal::Animal(const Animal &copy)
+{
+	this->type = copy.type;
+}
+
+Animal::~Animal()
+{
+}
+
+Animal& Animal::operator=(const Animal &assign)
+{
+	this->type = assign.type;
+	return *this;
+}
