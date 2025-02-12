415f0660ecc7f3725c110459364138e0de5e4dd5
Vidus Sivanathan <148643381+v1dusss@users.noreply.github.com>
2025-02-12 16:12:38 +0100

    cpp04: ex03: Add Character and ICharacter classes with basic structure

---
+++ cpp_04/ex03/include/Character.hpp
@@ -0,0 +1,24 @@
+#pragma once
+
+#include <iostream>
+#include "AMateria.hpp"
+#include "ICharacter.hpp"
+
+class Character : public ICharacter
+{
+private:
+	std::string _name;
+	AMateria* _materias[4];
+	
+public:
+	Character();
+	Character(std::string const & name);
+	Character(Character const & other);
+	virtual ~Character();
+	Character& operator=(Character const & other);
+
+	std::string const & getName() const;
+	void use(int idx, ICharacter& target);
+	void equip(AMateria* m);
+	void unequip(int idx);
+}
