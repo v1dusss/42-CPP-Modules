589324c64272bd54c0535f83baac33fb4167420a
Vidus Sivanathan <148643381+v1dusss@users.noreply.github.com>
2025-02-17 18:36:23 +0100

    cpp05: ex00: Bureaucrat add exception classes

--- cpp_05/ex00/include/Bureaucrat.hpp
+++ cpp_05/ex00/include/Bureaucrat.hpp
@@ -6,18 +6,32 @@ class Bureaucrat
 {
 private:
 	std::string const _name;
-	int _grade;
+	unsigned int _grade;
+	Bureaucrat() = delete;
 
 public:
-	Bureaucrat();
-	Bureaucrat(std::string const name, int grade);
+	Bureaucrat(std::string const &name, unsigned int grade);
 	Bureaucrat(Bureaucrat const &other);
 	~Bureaucrat();
 
 	Bureaucrat &operator=(Bureaucrat const &other);
 
 	std::string const &getName() const;
-	std::string const &getGrade() const;
+	unsigned int &getGrade() const;
 	void incrementGrade();
 	void decrementGrade();
+
+	class GradeTooHighException : public std::exception
+	{
+		public:
+			virtual const char *what() const noexcept override;
+	};
+
+	class GradeTooLowException : public std::exception
+	{
+		public:
+			virtual const char *what() const noexcept override;
+	};
 };
+
+std::ostream &operator<<(std::ostream &out, Bureaucrat const &bureaucrat);
