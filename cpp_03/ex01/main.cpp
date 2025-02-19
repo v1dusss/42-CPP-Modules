bf2304aaec4f9d172a75e06c876f76ab1b2f3815
Vidus Sivanathan <148643381+v1dusss@users.noreply.github.com>
2025-02-19 15:26:48 +0100

    cpp03: ex01: Add additional ScavTrap instance and demonstrate functionality

--- cpp_03/ex01/main.cpp
+++ cpp_03/ex01/main.cpp
@@ -2,6 +2,7 @@
 
 int main()
 {
+	ScavTrap scavTrap0;
 	ScavTrap scavTrap("ScavTrap_1");
 	ScavTrap scavTrap2("ScavTrap_2");
 	ScavTrap scavTrap3("ScavTrap_3");
@@ -9,6 +10,9 @@ int main()
 	scavTrap.attack("ScavTrap_2");
 	scavTrap2.takeDamage(20);
 	scavTrap.guardGate();
+	scavTrap0.attack("ScavTrap_2");
+	scavTrap2.takeDamage(20);
 	scavTrap3.attack("ScavTrap_2");
+	scavTrap2.beRepaired(10);
 	return 0;
 }
