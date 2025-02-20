ff604e22442e782f750530563e9afdbd6b63903f
Vidus Sivanathan <148643381+v1dusss@users.noreply.github.com>
2025-02-20 12:24:12 +0100

    cpp04: ex03: Implement AMateria class with constructor, destructor, and member functions

---
+++ cpp_04/ex03/src/AMateria.cpp
@@ -0,0 +1,28 @@
+#include "AMateria.hpp"
+
+AMateria::AMateria(const std::string& type) : _type(_type)
+{
+}
+
+AMateria::~AMateria()
+{
+}
+
+AMateria::AMateria(const AMateria& other) : _type(other._type)
+{
+}
+
+AMateria& AMateria::operator=(const AMateria& other)
+{
+	if (this != &other) {
+		_type = other._type;
+	}
+	return *this;
+}
+
+const std::string& AMateria::getType() const { return _type; }
+
+void AMateria::use(ICharacter& target)
+{
+	(void)target;
+}
