8029ccbe343bf393fb14bdb3311dac29ceffe5ef
Vidus Sivanathan <148643381+v1dusss@users.noreply.github.com>
2025-02-24 20:08:05 +0100

    cpp04: ex00: Enhance main function to include additional Animal and WrongAnimal instances

--- cpp_04/ex00/src/main.cpp
+++ cpp_04/ex00/src/main.cpp
@@ -5,25 +5,40 @@
 int main()
 {
 	Animal bob;
+	Cat jim;
+	Dog tim;
 	const Animal* meta = new Animal();
 	const Animal* dog = new Dog();
 	const Animal* cat = new Cat();
-	const WrongAnimal* w = new WrongCat();
+	const WrongAnimal* wrong_cat = new WrongCat();
 	std::cout << "-------------------------------" << std::endl;
-	std::cout << dog->getType() << " " << std::endl;
-	std::cout << cat->getType() << " " << std::endl;
-	std::cout << w->getType() << " " << std::endl;
-	std::cout << "-------------------------------" << std::endl;
-	cat->makeSound(); //will output the cat sound!
-	dog->makeSound();
-	w->makeSound();
-	meta->makeSound();
+	std::cout << "meta: " << meta->getType() << std::endl;
+	std::cout << "dog: " << dog->getType() << std::endl;
+	std::cout << "cat: " << cat->getType() << std::endl;
+	std::cout << "wrong_cat: " << wrong_cat->getType() << std::endl;
+	std::cout << "bob: " << bob.getType() << std::endl;
+	std::cout << "jim: " << jim.getType() << std::endl;
+	std::cout << "tim: " << tim.getType() << std::endl;
 
+	std::cout << "-------------------------------" << std::endl;
+	std::cout << "meta: ";
+	meta->makeSound();
+	std::cout << "dog: ";
+	dog->makeSound();
+	std::cout << "cat: ";
+	cat->makeSound();
+	std::cout << "wrong_cat: ";
+	wrong_cat->makeSound();
+	std::cout << "bob: ";
 	bob.makeSound();
+	std::cout << "jim: ";
+	jim.makeSound();
+	std::cout << "tim: ";
+	tim.makeSound();
 	std::cout << "-------------------------------" << std::endl;
 	delete meta;
 	delete dog;
 	delete cat;
-	delete w;
+	delete wrong_cat;
 	return 0;
 }
