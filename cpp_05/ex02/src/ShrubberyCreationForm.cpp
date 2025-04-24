3e03b95aa9d0f1caa043990b044cdf5166d88e04
Vidus Sivanathan <148643381+v1dusss@users.noreply.github.com>
2025-04-24 13:29:50 +0200

    cpp05: ex02: added ASCII art of a shrubbery

--- cpp_05/ex02/src/ShrubberyCreationForm.cpp
+++ cpp_05/ex02/src/ShrubberyCreationForm.cpp
@@ -18,7 +18,6 @@ ShrubberyCreationForm& ShrubberyCreationForm::operator=(ShrubberyCreationForm co
 
 void ShrubberyCreationForm::execute(Bureaucrat const &executor) const 
 {
-	std::cout << "execute" << std::endl;
 	if (!this->isSigned())
 		throw AForm::FormNotSignedException();
 	if (executor.getGrade() > this->getGradeToExecute())
@@ -30,7 +29,27 @@ void ShrubberyCreationForm::execute(Bureaucrat const &executor) const
 		std::cerr << "Error: could not open file " << _target + "_shrubbery" << std::endl;
 		return;
 	}
-	file << "ASCII art of a shrubbery" << std::endl;
+	file << "                     .o00o" << std::endl;
+	file << "                   o000000oo" << std::endl;
+	file << "                  00000000000o" << std::endl;
+	file << "                 00000000000000" << std::endl;
+	file << "              oooooo  00000000  o88o" << std::endl;
+	file << "           ooOOOOOOOoo  ```''  888888" << std::endl;
+	file << "         OOOOOOOOOOOO'.qQQQQq. `8888'" << std::endl;
+	file << "        oOOOOOOOOOO'.QQQQQQQQQQ/.88'" << std::endl;
+	file << "        OOOOOOOOOO'.QQQQQQQQQQ/ /q" << std::endl;
+	file << "         OOOOOOOOO QQQQQQQQQQ/ /QQ" << std::endl;
+	file << "           OOOOOOOOO `QQQQQQ/ /QQ'" << std::endl;
+	file << "             OO:F_P:O `QQQ/  /Q'" << std::endl;
+	file << "                \\\\. \\\\ |  // |" << std::endl;
+	file << "                d\\\\ \\\\\\\\|_////" << std::endl;
+	file << "                qP| \\\\ _' `|Ob" << std::endl;
+	file << "                   \\\\  / \\\\  \\\\Op" << std::endl;
+	file << "                   |  | O| |" << std::endl;
+	file << "           _       /\\\\. _/ /|." << std::endl;
+	file << "            `---__/|_\\\\   /|/|  __" << std::endl;
+	file << "                  `-'  `-'`-'-'" << std::endl;
+
 	file.close();
 
 	std::cout << _target + "_shrubbery" << " created" << std::endl;
