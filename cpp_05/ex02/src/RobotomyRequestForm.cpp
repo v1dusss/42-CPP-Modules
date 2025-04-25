ea4cb81c511a8580079b89fcc59add355546ba4c
Vidus Sivanathan <148643381+v1dusss@users.noreply.github.com>
2025-04-25 14:44:54 +0200

    cpp05: ex02: added RobotomyRequestForm execution

--- cpp_05/ex02/src/RobotomyRequestForm.cpp
+++ cpp_05/ex02/src/RobotomyRequestForm.cpp
@@ -2,7 +2,7 @@
 #include "Bureaucrat.hpp"
 #include "AForm.hpp"
 
-RobotomyRequestForm::RobotomyRequestForm(std::string const &target) : AForm("RobotomyRequestForm", 145, 137), _target(target)
+RobotomyRequestForm::RobotomyRequestForm(std::string const &target) : AForm("RobotomyRequestForm", 72, 45), _target(target)
 {}
 
 RobotomyRequestForm::~RobotomyRequestForm()
@@ -17,4 +17,15 @@ RobotomyRequestForm& RobotomyRequestForm::operator=(RobotomyRequestForm const &o
 
 void RobotomyRequestForm::execute(Bureaucrat const &executor) const 
 {
+	if (!this->isSigned())
+		throw AForm::FormNotSignedException();
+	if (executor.getGrade() > this->getGradeToExecute())
+		throw AForm::GradeTooLowException();
+	
+	std::cout << "* drilling noises *" << std::endl;
+
+	if (rand() % 2)
+		std::cout << _target << " has been robotomized successfully" << std::endl;
+	else
+		std::cout << "Robotomy failed for " << _target << std::endl;
 }
