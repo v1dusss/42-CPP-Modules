23571f2ac6ad08a80a48daf68fc4577a6b504c93
Vidus Sivanathan <148643381+v1dusss@users.noreply.github.com>
2025-06-22 20:41:11 +0200

    cpp07: ex00: Update main implementation to use a larger vector and change search target in easyfind

--- cpp_08/ex00/src/main.cpp
+++ cpp_08/ex00/src/main.cpp
@@ -3,10 +3,10 @@
 #include "easyfind.hpp"
 
 int main() {
-    std::vector<int> vec = {10, 20, 30, 40};
+    std::vector<int> vec = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
 
     try {
-        std::vector<int>::iterator it = easyfind(vec, 30);
+        std::vector<int>::iterator it = easyfind(vec, 5);
         std::cout << "Found: " << *it << std::endl;
     } catch (const std::exception& e) {
         std::cerr << e.what() << std::endl;
