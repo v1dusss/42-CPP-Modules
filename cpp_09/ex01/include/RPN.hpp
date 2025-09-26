f624b81cc40d3eec140db3e8aabfdf4e93535ebe
v1dusss <sivanathan.vidus@gmail.com>
2025-09-26 19:55:58 +0200

    Implement RPN class with calculation logic and input validation

--- cpp_09/ex01/include/RPN.hpp
+++ cpp_09/ex01/include/RPN.hpp
@@ -1,6 +1,19 @@
 #pragma once
 
+#include <stack>
+
 
 class RPN {
+	private:
+		std::string input;
+		std::stack<int> buffer;
 
+		bool validate(std::string str);
+	public:
+		RPN();
+		RPN(const RPN& copy);
+		RPN& operator=(const RPN& other);
+		~RPN();
+
+		int calculate_RPN(std::string str);
 };
---
+++ cpp_09/ex01/src/RPN.cpp
@@ -0,0 +1,67 @@
+#include "RPN.hpp"
+
+RPN::RPN() {}
+
+RPN::RPN(const RPN& copy)
+{
+	this->input = copy.input;
+	this->buffer = copy.buffer;
+}
+
+RPN::~RPN() {}
+
+RPN& RPN::operator=(const RPN& other)
+{
+	if (this != &other)
+	{
+		this->input = other.input;
+		this->buffer = other.buffer;
+	}
+	return *this;
+}
+
+int RPN::calculate_RPN(std::string str)
+{
+	for (int i = 0; i < str.length(); i++)
+	{
+		if (str[i] == ' ')
+			continue;
+		else if (str[i] >= '0' && str[i] <= '9')
+			buffer.push(str[i] - '0');
+		else if (str[i] == '+' || str[i] == '-' || str[i] == '*' || str[i] == '/')
+		{
+			if (buffer.size() < 2)
+				throw std::runtime_error("Error: number is missing");
+			int b = buffer.top();
+			buffer.pop();
+			int a = buffer.top();
+			buffer.pop();
+			switch (str[i])
+			{
+				case '+':
+					buffer.push(a + b);
+					break;
+				case '-':
+					buffer.push(a - b);
+					break;
+				case '*':
+					buffer.push(a * b);
+					break;
+				case '/':
+					buffer.push(a / b);
+					break;
+				default:
+					throw std::runtime_error("Error: Invalid operator");
+			}
+		}
+		else
+			throw std::runtime_error("Error: Invalid character");
+	}
+
+	if (buffer.size() < 1)
+		throw std::runtime_error("Error: number is missing");
+	if (buffer.size() > 1)
+		throw std::runtime_error("Error: too many numbers");
+
+	return buffer.top();
+}
