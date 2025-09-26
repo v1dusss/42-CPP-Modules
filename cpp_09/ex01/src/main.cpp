419b6279e6efd59988188a1d36572dab289fec43
v1dusss <sivanathan.vidus@gmail.com>
2025-09-26 20:02:53 +0200

    Implement main function for RPN calculator

--- cpp_09/ex01/src/main.cpp
+++ cpp_09/ex01/src/main.cpp
@@ -1,7 +1,20 @@
 #include <iostream>
 #include "RPN.hpp"
 
-int main()
+int main(int argc, char** argv)
 {
-    return 0;
+	RPN rpn;
+
+	if (argc != 2) {
+		std::cerr << "Usage: " << argv[0] << " <RPN_expression>" << std::endl;
+		return 1;
+	}
+
+	try {
+		int result = rpn.calculate_RPN(argv[1]);
+		std::cout << "Result: " << result << std::endl;
+	} catch (const std::exception& e) {
+		std::cerr << e.what() << std::endl;
+	}
+	return 0;
 }
